0a494be4e1d91439a5df68b86eabef65aa51d6b5
jy Kim <0008mari@gmail.com>
2022-07-21 15:36:35 +0900

    001. 개발 환경 세팅

---
+++ src/main/java/cherrytea/soona/SoonaApplication.java
@@ -0,0 +1,13 @@
+package cherrytea.soona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cherrytea/soona/SoonaApplicationTests.java
@@ -0,0 +1,13 @@
+package cherrytea.soona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
